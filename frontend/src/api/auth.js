e2990000337d51e9cf658a447af8c3760a1c1910
rayyanqureshi0123 <rayyanqureshi1112@gmail.com>
2026-04-09 01:16:44 +0530

    frontend

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,4 +1,4 @@
 import API from './axios';
 
-export const loginAPI = (data) => API.post('/auth/login', data);
-export const signupAPI = (data) => API.post('/auth/signup', data);
+export const loginAPI = (data) => API.post('api/auth/login', data);
+export const signupAPI = (data) => API.post('api/auth/signup', data);
--- frontend/src/api/expenses.js
+++ frontend/src/api/expenses.js
@@ -1,10 +1,10 @@
 import API from './axios';
 
-export const getExpensesAPI = (params) => API.get('/expenses', { params });
-export const addExpenseAPI = (data) => API.post('/expenses', data);
-export const updateExpenseAPI = (id, data) => API.put(`/expenses/${id}`, data);
-export const deleteExpenseAPI = (id) => API.delete(`/expenses/${id}`);
+export const getExpensesAPI = (params) => API.get('api/expenses', { params });
+export const addExpenseAPI = (data) => API.post('api/expenses', data);
+export const updateExpenseAPI = (id, data) => API.put(`api/expenses/${id}`, data);
+export const deleteExpenseAPI = (id) => API.delete(`api/expenses/${id}`);
 
-export const getMonthlySummaryAPI = () => API.get('/expenses/summary/monthly');
-export const getYearlySummaryAPI = () => API.get('/expenses/summary/yearly');
-export const getCategorySummaryAPI = () => API.get('/expenses/summary/category');
+export const getMonthlySummaryAPI = () => API.get('api/expenses/summary/monthly');
+export const getYearlySummaryAPI = () => API.get('api/expenses/summary/yearly');
+export const getCategorySummaryAPI = () => API.get('api/expenses/summary/category');
